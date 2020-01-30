b4883a806bb428eb0d515e56c2ed54b509555d0f
cesar <fernandocheroayasta@outlook.com>
2020-01-30 06:26:41 -0500

    mi primer commit

---
+++ clase 1/tipo-dato.js
@@ -0,0 +1,12 @@
+let numero=5
+console.log(numero);
+let nombre="cesar chero";
+console.log(nombre);
+let respuesta=false
+console.log(respuesta);
+
+const pi=3.1415;
+console.log(pi);
+
+numero=99;
+console.log(numero);
