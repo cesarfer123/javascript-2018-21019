81c5e8db8929f292b666617507b8dbc2c5e1fd75
cesar <fernandocheroayasta@outlook.com>
2020-02-05 16:28:05 -0500

    Expresiones Regulares

---
+++ clase 44/expresion-regular.js
@@ -0,0 +1,8 @@
+const text=document.getElementById("text").textContent
+
+const regEx= /lorem/gi
+const regEx2=new RegExp("lorem","gi")
+const regEx2=new RegExp("/lorem/ ","gi")
+
+console.log(regEx.test(text))
+console.log(text.includes("Lorem"));
