efc6f9e158c444c2cadcbe04c09581647e013cb1
cesar <fernandocheroayasta@outlook.com>
2020-02-06 08:02:48 -0500

    Api File II

---
+++ clase 53/api-fileII.js
@@ -0,0 +1,39 @@
+const fileInput=document.getElementById("file")
+const progress=document.getElementById("progress")
+
+/*
+fileInput.addEventListener("change",(e)=>{
+
+    const file=e.target.files[0]
+    const fileReader=new FileReader()
+    fileReader.readAsDataURL(file)
+
+    fileReader.addEventListener("progress",(e)=>{
+
+        progress.style.width=Number.parseInt(e.loaded*100/e.total) + "%"
+        // console.log(e.loaded*100/e.total)
+        // console.log(e.total)
+    })
+
+    fileReader.addEventListener("loadend",(e)=>{
+        progress.style.width="100%"
+    })
+})*/
+
+const root=document.documentElement
+
+fileInput.addEventListener("change",(e)=>{
+
+    const file=e.target.files[0]
+    const fileReader=new FileReader()
+    fileReader.readAsDataURL(file)
+
+    fileReader.addEventListener("progress",(e)=>{
+
+        root.style.setProperty("--bar-width",Number.parseInt(e.loaded*100/e.total) + "%")       
+    })
+
+    fileReader.addEventListener("loadend",(e)=>{
+        root.style.setProperty("--bar-width","100%")
+    })
+})
